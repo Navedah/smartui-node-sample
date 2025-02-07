439f9aef3ac60279b183ff5c8a01e33ecbe6fa40
Navedah <122727800+Navedah@users.noreply.github.com>
2025-02-07 16:33:12 +0530

    Update sdkCloud.js

--- sdk/sdkCloud.js
+++ sdk/sdkCloud.js
@@ -4,7 +4,7 @@ const { smartuiSnapshot } = require('@lambdatest/selenium-driver');
 // username: Username can be found at automation dashboard
 const USERNAME = process.env.LT_USERNAME || "<USERNAME>";
 
-// AccessKey:  AccessKey can be generated from automation dashboard or profile section
+// AccessKey:  AccessKey can be generated from automation dashboard or profile section comment
 const KEY = process.env.LT_ACCESS_KEY || "<ACCESS_KEY>";
 let capabilities = {
   platform: "catalina",
